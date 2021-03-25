50c32d1b26c702b647cb5a763d6f29553b2ed15b
exion89 <samuel.lundy@gmail.com>
2021-03-24 23:33:31 -0600

    Updated nc-xpath-py

--- Python/Networking/IOS-XE/scrapli-yang/nc-xpath.py
+++ Python/Networking/IOS-XE/scrapli-yang/nc-xpath.py
@@ -1,9 +1,9 @@
 from scrapli_netconf.driver import NetconfScrape
 
 my_device = {
-    "host": "10.10.21.15",
-    "auth_username": "cisco",
-    "auth_password": "cisco",
+    "host": "sandbox-iosxe-latest-1.cisco.com",
+    "auth_username": "developer",
+    "auth_password": "C1sco12345",
     "auth_strict_key": False,
     "port": 830
 }
@@ -11,7 +11,7 @@ my_device = {
 conn = NetconfScrape(**my_device)
 conn.open()
 
-ospf_xpath = '/ospf-oper-data/ospf-state/ospf-instance[af="address-family-ipv4" and router-id="16843009"]/ospf-area[area-id=0]/ospf-interface[name="GigabitEthernet2"]/ospf-neighbor[neighbor-id="2.2.2.2"]/state'
+ospf_xpath = '/ospf-oper-data/ospf-state/ospf-instance[af="address-family-ipv4" and router-id="235802126"]/ospf-area[area-id=599]/ospf-interface[name="Loopback14"]'
 response = conn.get(
     filter_=ospf_xpath, filter_type='xpath')
 print(response.result)
